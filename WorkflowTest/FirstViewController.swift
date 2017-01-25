62a5ea72a42bf32f6adc2ac9c5dd766d631efd30
Julian Kühn <juliankuhn@Julians-MacBook-Pro.local>
2017-01-25 16:10:55 +0100

    Initial Commit

---
+++ WorkflowTest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WorkflowTest
+//
+//  Created by Julian Kühn on 25.01.17.
+//  Copyright © 2017 Julian Kühn. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
